2554155b91e14c684a668ba80bba5aad58842bea
Dhruvi Jadeja <193613037+dhruvijadejaa@users.noreply.github.com>
2026-07-24 11:37:43 +0530

    new update 2

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import CompanionForm from "@/components/CompanionForm"
+
 
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
+    <main className="flex justify-center items-center lg:w-1/3 md:w-2/3">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Link from "next/link"
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className='cta-section'>
+      <div className='cta-badge'>Start Learning Your Way.</div>
+      <h2>Build and Personalize Learning Companion</h2>
+      <p>Pick a name, subject, voice & personality - and start learning through voice conversations that feel natural and fun.</p>
+      <img src="images/cta.svg" alt=""
+      width={362} height={232}
+      />
+      <button className="btn-primary cursor-pointer">
+        <img src="/icons/plus.svg" alt="" />
+        <Link href='/companions/new'>
+        <p>Build a new Companion</p>
+        </Link>
+      </button>
+    </section>
   )
 }
 
